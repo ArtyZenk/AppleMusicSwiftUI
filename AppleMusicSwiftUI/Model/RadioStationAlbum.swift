0d6f227d2b8c2328996103923e0e84f2d4257421
Artyom <artyomka13@gmail.com>
2022-04-18 10:53:20 +0300

    add content for model

--- AppleMusicSwiftUI/Model/RadioStationAlbum.swift
+++ AppleMusicSwiftUI/Model/RadioStationAlbum.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct RadioStations: Hashable {
+    let name: String
+    let description: String
+
+    var coverImageStation: Image {
+        Image(name)
+    }
+}
+
+func getRadioStations() -> [RadioStations] {
+    [
+        RadioStations(name: "BounceFM", description: "Фанк радиостанция"),
+        RadioStations(name: "CSRFM", description: "Инструментальная музыка"),
+        RadioStations(name: "JulioG", description: "Классический рок"),
+        RadioStations(name: "KDSTFM", description: "Техно радиостаниця"),
+        RadioStations(name: "KJANFM", description: "Регги музыка"),
+        RadioStations(name: "KROSEFM", description: "Классическая музыка"),
+        RadioStations(name: "PlayBackFM", description: "HIP-HOP"),
+        RadioStations(name: "RadioX", description: "Разговорный жанр")
+    ]
+}
